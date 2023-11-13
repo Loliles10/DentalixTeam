dafd745931025501c98c97792fe6102cb41f3fcf
prf0003 <prf0003@alu.medac.es>
2023-11-13 10:18:56 +0100

    ventanaDoctorInterna

--- src/mainPack/HeaderTabla.java
+++ src/mainPack/HeaderTabla.java
@@ -9,6 +9,8 @@ import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
 
 public class HeaderTabla extends DefaultTableCellRenderer {
+	public HeaderTabla() {
+	}
 
     private static final long serialVersionUID = 1L;
 
